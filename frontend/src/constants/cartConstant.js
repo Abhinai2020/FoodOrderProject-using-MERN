4a7a59082ac0c95b73fd2ee50516bb904edd5a30
Abhinai2020 <abhishekvilladin@gmail.com>
2024-08-15 22:55:20 +0530

    Add FoodOrder Project

---
+++ frontend/src/constants/cartConstant.js
@@ -0,0 +1,5 @@
+export const ADD_TO_CART="ADD_TO_CART";
+export const UPDATE_CARD_ITEM="UPDATE_CARD_ITEM";
+export const REMOVE_ITEM_CART="REMOVE_ITEM_CART";
+export const FETCH_CART="FETCH_CART";
+export const CLEAR_CART="CLEAR_CART";
